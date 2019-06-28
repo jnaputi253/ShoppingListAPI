29cc830a07acdb8166c70ef48fac8d19c5d96721
jnaputi <jnaputi253@gmail.com>
2019-06-27 22:59:40 -0500

    Initial

---
+++ ShoppingListAPI.Tests/Controllers/CategoriesControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using ShoppingListAPI.Models;
+using ShoppingListAPI.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ShoppingListAPI.Controllers;
+using ShoppingListAPI.Services;
+using Xunit;
+
+namespace ShoppingListAPI.Tests.Controllers
+{
+    public class CategoriesControllerTests
+    {
+        private IRepository<Category> _repository;
+        private IService<Category> _service;
+        private CategoriesController _controller;
+
+        [Fact]
+        public async void ShouldReturnNoCategories()
+        {
+            var mockRepository = new Mock<IRepository<Category>>();
+            mockRepository.Setup(mock => mock.FetchAll())
+                .ReturnsAsync(new List<Category>());
+
+            _repository = mockRepository.Object;
+            _service = new CategoryService(_repository);
+            _controller = new CategoriesController(_service);
+
+
+            IActionResult result = await _controller.FetchAllCategories();
+
+
+            result.Should().BeOfType<ObjectResult>()
+                .Which.Value.Should().BeAssignableTo<IEnumerable<Category>>()
+                .Which.Count().Should().Be(0);
+        }
+
+        [Fact]
+        public async void ShouldReturnTheCorrectAmountOfCategories()
+        {
+            var categories = new List<Category>
+            {
+                Mock.Of<Category>(),
+                Mock.Of<Category>(),
+                Mock.Of<Category>()
+            };
+
+            var mockRepository = new Mock<IRepository<Category>>();
+            mockRepository.Setup(mock => mock.FetchAll())
+                .ReturnsAsync(categories);
+
+            _repository = mockRepository.Object;
+            _service = new CategoryService(_repository);
+            _controller = new CategoriesController(_service);
+
+
+            IActionResult result = await _controller.FetchAllCategories();
+
+
+            result.Should().BeOfType<ObjectResult>()
+                .Which.Value.Should().BeAssignableTo<IEnumerable<Category>>()
+                .Which.Count().Should().Be(3);
+        }
+    }
+}
---
+++ ShoppingListAPI/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using ShoppingListAPI.Models.Database;
+
+namespace ShoppingListAPI.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement(CategorySchema.Name)]
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }
+    }
+}
